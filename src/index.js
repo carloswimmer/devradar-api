915fad7306fc7bc17e144fda40146ff7d43996d1
carloswimmer <coiote777@gmail.com>
2020-01-19 16:24:45 -0300

    feat: Implement websocket

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const http = require('http')
+
 const routes = require('./routes')
+const { setupWebsocket } = require('./websocket')
 require('dotenv/config')
 
 const app = express()
-
+const server = http.Server(app)
 const { DB_NAME, DB_PASS, DB_CLUSTER, API_PORT } = process.env
+
+setupWebsocket(server)
 
 mongoose.connect(`mongodb+srv://${DB_NAME}:${DB_PASS}@${DB_CLUSTER}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
@@ -18,4 +23,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(API_PORT)
+server.listen(API_PORT)
